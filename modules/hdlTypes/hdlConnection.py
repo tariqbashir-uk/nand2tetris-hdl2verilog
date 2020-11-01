8fcb80650f4699bfc9a607d64703a4b2523717ef
Tariq Bashir <tbashir76@gmail.com>
2020-11-01 22:35:05 +0000

    Tidy up HdlConnection and add bit params for pin1

--- modules/hdlTypes/hdlConnection.py
+++ modules/hdlTypes/hdlConnection.py
@@ -8,18 +8,11 @@ class HdlConnection():
         self.pin1 = pin1
         self.pin2 = pin2
 
-        self.pin2BitIndex      = commonDefs.NO_BIT_VALUE
-        self.pin2StartBitOfBus = commonDefs.NO_BIT_VALUE
-        self.pin2EndBitOfBus   = commonDefs.NO_BIT_VALUE
-
-        bitWidthString = self.pin2.GetPinBitWidthString()
-        if bitWidthString:
-            if ".." in bitWidthString:
-                bitValues = bitWidthString.split("..")
-                self.pin2StartBitOfBus = int(bitValues[0])
-                self.pin2EndBitOfBus   = int(bitValues[1])
-            else:
-                self.pin2BitIndex      = int(bitWidthString)         
+        self.pin1BitIndex, self.pin1StartBitOfBus, self.pin1EndBitOfBus = (
+            self._SetPinBitParams(self.pin1.GetPinBitWidthString()))
+      
+        self.pin2BitIndex, self.pin2StartBitOfBus, self.pin2EndBitOfBus = (
+            self._SetPinBitParams(self.pin2.GetPinBitWidthString()))
         return
 
     ##########################################################################
@@ -29,12 +22,35 @@ class HdlConnection():
     ##########################################################################
     def GetPinStr(self):
         pin1Name  = self.pin1.pinName
-        pin2Name  = self.pin2.pinName
-        pin2Extra = ""
+        pin1Extra = self._GetPinExtraStr(self.pin1BitIndex, self.pin1StartBitOfBus, self.pin1EndBitOfBus)
 
-        if self.pin2BitIndex != commonDefs.NO_BIT_VALUE:
-            pin2Extra += "[" + str(self.pin2BitIndex) + "]"
-        elif self.pin2StartBitOfBus != commonDefs.NO_BIT_VALUE:
-            pin2Extra += "[" + str(self.pin2StartBitOfBus) + ".." + str(self.pin2EndBitOfBus) + "]"
+        pin2Name  = self.pin2.pinName        
+        pin2Extra = self._GetPinExtraStr(self.pin2BitIndex, self.pin2StartBitOfBus, self.pin2EndBitOfBus)
 
-        return ("[%s : %s%s (%s)]" % (pin1Name, pin2Name, pin2Extra, self.pin2.pinType)) 
+        return ("[%s%s : %s%s (%s)]" % (pin1Name, pin1Extra, pin2Name, pin2Extra, self.pin2.pinType))
+
+    ##########################################################################
+    def _SetPinBitParams(self, bitWidthString):
+        pinBitIndex      = commonDefs.NO_BIT_VALUE
+        pinStartBitOfBus = commonDefs.NO_BIT_VALUE
+        pinEndBitOfBus   = commonDefs.NO_BIT_VALUE
+
+        if bitWidthString:
+            if ".." in bitWidthString:
+                bitValues = bitWidthString.split("..")
+                pinStartBitOfBus = int(bitValues[0])
+                pinEndBitOfBus   = int(bitValues[1])
+            else:
+                pinBitIndex      = int(bitWidthString)   
+
+        return pinBitIndex, pinStartBitOfBus, pinEndBitOfBus
+
+    ##########################################################################
+    def _GetPinExtraStr(self, pinBitIndex, pinStartBitOfBus, pinEndBitOfBus):
+        pinExtraStr = ""
+        if pinBitIndex != commonDefs.NO_BIT_VALUE:
+            pinExtraStr += "[" + str(pinBitIndex) + "]"
+        elif pinStartBitOfBus != commonDefs.NO_BIT_VALUE:
+            pinExtraStr += "[" + str(pinStartBitOfBus) + ".." + str(pinEndBitOfBus) + "]"
+        return pinExtraStr
+
