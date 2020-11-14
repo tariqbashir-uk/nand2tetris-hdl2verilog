747a0f66bf3800155a87cd79a3ea46f60b9d02bd
Tariq Bashir <tbashir76@gmail.com>
2020-11-14 23:17:39 +0000

    Update test all script to convert test files and run generated test cases

--- test_modules/test_all.sh
+++ test_modules/test_all.sh
@@ -5,8 +5,19 @@ fi
 
 list=$(find . -name '*.tst')
 
+echo "############# Checking test modules pass on nand2fpga Hardware Simulator #############"
 for testFile in $list
 do
   echo "Testing $testFile" 
   "${NAND2_FPGA_HARDWARESIM}" $testFile
 done
+
+echo "############# Converting modules #############"
+cd ..
+python3 ./hdl2verilog.py -i ./test_modules -o ./tmp_generated
+
+cd tmp_generated
+chmod +x ./runme.sh
+
+echo "############# Running generated tests #############"
+./runme.sh
