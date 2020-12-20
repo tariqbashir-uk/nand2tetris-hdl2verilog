d9869341476e78a02de3ce66729c56e7ff58e7b5
Tariq Bashir <tbashir76@gmail.com>
2020-12-20 23:58:56 +0000

    Fix for built-in 4k ram module.

--- builtin_chips/BuiltIn_RAM64.v
+++ builtin_chips/BuiltIn_RAM64.v
@@ -3,7 +3,7 @@ module RAM64(in, load, address, clk, out);
     input        load;
     input [5:0]  address;
     input        clk;
-    output reg [15:0] out;
+    output [15:0] out;
 
     reg [15:0] mem [63:0];
 
@@ -12,12 +12,11 @@ module RAM64(in, load, address, clk, out);
         for (i=0; i < 64; i=i+1) begin
             mem[i] <= 16'b0; //reset array
         end
-        out <= 16'b0;
     end
 
     always @(posedge clk) begin
         if (load)
-            mem[address] = in;
-        out <= mem[address];
+            mem[address] <= in;
     end
+    assign out = mem[address];
 endmodule
